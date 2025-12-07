8f28ad017bc23c64e9a93db5aa33fdac2b827e1f
Florin Ciurcanu <florin.ciurcanu@gmail.com>
2025-12-07 09:41:07 +0200

    this looks impossible

--- codingame/seti_encoder/decode.py
+++ codingame/seti_encoder/decode.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python3
 
+from typing import Sequence
 
-def log_bin(values: list, size: int, name="", prefix=""):
+
+def log_array(values: Sequence, size: int, name="", prefix=""):
     if name:
         print(f"{name.title()}")
 
     for value in values[: size // 16]:
-        print(f"{prefix}{value:032b} {value:08x}x // {value} ({value.bit_length()})")
-
-
-def bin_to_grey(val: int):
-    return val ^ (val >> 1)
+        print(f"{value:08x}x  {prefix}{value:032b}  // ({value.bit_length():2} bits)")
 
 
 def expand(n: int) -> list[tuple]:
@@ -20,9 +18,7 @@ def expand(n: int) -> list[tuple]:
     return result
 
 
-
-
-def encode(data: list, size: int):
+def encode(data: Sequence, size: int):
     store_size = size // 16
     buffer = [0] * store_size
 
@@ -36,40 +32,58 @@ def encode(data: list, size: int):
             bit_j = (data[pos_j + size // 32] >> shift_j) & 1
             buffer[pos_out] ^= (bit_i & bit_j) << shift_out
 
-    return buffer
+    return buffer[:store_size]
 
 
-def mask(data: list, size: int):
+def mask(data: Sequence, size: int):
     store_size = size // 16
     buffer = [0] * store_size
 
-    count = [0] * (size * 2)
     for i in range(size):
         for j in range(size):
             pos_i, shift_i = divmod(i, 32)
             pos_j, shift_j = divmod(j, 32)
             pos_out, shift_out = divmod(i + j, 32)
 
-            count[i + j] += 1
+            bit = (data[pos_out] >> shift_out) & 1
+            if bit:
+                pass
+                # buffer[pos_i] |= bit << shift_i
+                # buffer[pos_j + size // 32] |= bit << shift_j
+            else:
+                buffer[pos_i] ^= (1 - bit) << shift_i
+                buffer[pos_j + size // 32] ^= (1 - bit) << shift_j
 
-    print(count)
     return buffer
 
 
 def main(filename: str):
+    print("--" * 20)
     with open(filename, "rt") as file:
         content = file.read().strip()
         print("read", len(content), "bytes from", filename)
 
     size, encoded = content.split("\n")
     size = int(size)
-    print(size, "bits")
+    print("Working with", size, "bits")
     encoded = [int(val, 16) for val in encoded.split()]
+    log_array(encoded, size, "read")
 
-    log_bin(encoded, size, "looking around")
-    mm = mask(encoded, size)
+    # brutte force
+    # for a in range(256):
+    #     for b in range(256):
+    #         data = (a, b)
+    #         result = encode(data, size)
+    #         if result == encoded:
+    #             log_array(data, size, "found")
 
-    log_bin(mm, size, "masked")
+    data = (0x83, 0xe5)
+    log_array(data, size, "sample")
+    result = encode(data, size)
+    log_array(result, size, "all ones")
+
+    mm = mask(result, size)
+    log_array(mm, size, "mask")
 
 
 if __name__ == "__main__":
