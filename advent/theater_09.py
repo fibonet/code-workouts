34a97d25a8a522a71d661cf2af3f9f05209da57a
Florin Ciurcanu <florin.ciurcanu@gmail.com>
2025-12-12 21:45:04 +0200

    Captured horizontal and vertical lines, they must lead to something

--- advent/theater_09.py
+++ advent/theater_09.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
-from collections import namedtuple
-from itertools import combinations
-from operator import attrgetter
+from collections import defaultdict, namedtuple
+from collections.abc import Generator
+from itertools import chain, combinations
+from pprint import pprint
+
+import matplotlib.pyplot as plt
 
 
 def banner(name: str):
@@ -47,24 +50,62 @@ def solve_largest(filename: str):
     return largest_area
 
 
-def find_largest_color(red_tiles: list[Coords], grid: list[list[str]]):
+def contour_walk(a: Coords, b: Coords) -> Generator[Coords]:
+    left = min(a.x, b.x)
+    right = max(a.x, b.x)
+    top = min(a.y, b.y)
+    bottom = max(a.y, b.y)
+
+    for i in range(left, right + 1):
+        yield Coords(i, top)
+
+    for i in range(top, bottom + 1):
+        yield Coords(right, i)
+
+    for i in range(left, right + 1):
+        yield Coords(i, bottom)
+
+    for i in range(top, bottom + 1):
+        yield Coords(left, i)
+
+
+def find_largest_color(red_tiles: list[Coords], green_tiles: list[Coords]):
     all_pairs = combinations(red_tiles, 2)
     largest_pair = next(all_pairs)
     largest_area = area(*largest_pair)
+
+    by_rows = defaultdict(list)
+    for ti in chain(red_tiles, green_tiles):
+        by_rows[ti.y].append(ti.x)
+
+    by_cols = defaultdict(list)
+    for ti in chain(red_tiles, green_tiles):
+        by_cols[ti.x].append(ti.y)
+
     for a, b in all_pairs:
-        if (
-            any(grid[a.y][ci] == "." for ci in range(a.x + 1, b.x))
-            or any(grid[b.y][ci] == "." for ci in range(a.x + 1, b.x))
-            or any(grid[ri][a.x] == "." for ri in range(a.y + 1, b.y))
-            or any(grid[ri][b.x] == "." for ri in range(a.y + 1, b.y))
-        ):
-            # check if all tiles on countour are empty
+        is_inside = True
+        for ti in contour_walk(a, b):
+            left, right = min(by_rows[ti.y]), max(by_rows[ti.y])
+            if ti.x < left or ti.x > right:
+                is_inside = False
+                print(ti, "not inside, by row")
+                break
+
+            top, bottom = min(by_cols[ti.x]), max(by_cols[ti.x])
+            if ti.y < top or ti.y > bottom:
+                is_inside = False
+                print(ti, "not inside, by col")
+                break
+
+        if not is_inside:
             continue
 
         new_area = area(a, b)
         if new_area > largest_area:
             largest_pair = (a, b)
             largest_area = new_area
+
+    print("largest", *largest_pair)
 
     return largest_area
 
@@ -75,40 +116,65 @@ def solve_colorized(filename: str):
         content = file.read().strip()
         print("read", len(content), "bytes from", filename)
 
-    tiles = [Coords(*map(int, line.split(","))) for line in content.split("\n")]
-    print("Parsed", len(tiles), "red tiles.")
+    red_tiles = [Coords(*map(int, line.split(","))) for line in content.split("\n")]
+    print("Parsed", len(red_tiles), "red tiles.")
 
-    rows = max(ti.y for ti in tiles) + 3
-    cols = max(ti.x for ti in tiles) + 3
-    print("Grid size", rows, "x", cols)
-
-    if max(rows, cols) > 10000:
-        raise ValueError(f"Grid of {rows:_} x {cols:_} = {rows * cols:_} is too big.")
-
-    grid = [["."] * cols for _ in range(rows)]
+    rows = max(ti.y for ti in red_tiles) + 3
+    cols = max(ti.x for ti in red_tiles) + 3
 
     # scan rows
-    tiles.sort(key=lambda ti: (ti.y, ti.x))
-    contour = tiles.copy()
-    last = tiles[0]
-    for ti in tiles[1:]:
+    red_tiles.sort(key=lambda ti: (ti.y, ti.x))
+    green_tiles = list()
+    last = red_tiles[0]
+    horizontal = list()
+    left, right = last.x, last.x
+    for ti in red_tiles[1:]:
         if ti.y == last.y:
             for ci in range(last.x + 1, ti.x):
-                contour.append(Coords(ci, ti.y))
+                green_tiles.append(Coords(ci, ti.y))
+            left, right = min(ti.x, left), max(ti.x, right)
+        else:
+            horizontal.append((Coords(left, last.y), Coords(right, last.y)))
+            left, right = ti.x, ti.x
+
         last = ti
+    horizontal.append((Coords(left, last.y), Coords(right, last.y)))
+
+    print("Horizontal")
+    print(*horizontal, sep="\n")
 
     # scan columns
-    tiles.sort(key=lambda ti: (ti.x, ti.y))
-    last = tiles[0]
-    for ti in tiles[1:]:
+    red_tiles.sort(key=lambda ti: (ti.x, ti.y))
+    last = red_tiles[0]
+    vertical = list()
+    top, bottom = last.y, last.y
+    for ti in red_tiles[1:]:
         if ti.x == last.x:
             for ri in range(last.y + 1, ti.y):
-                contour.append(Coords(ti.x, ri))
+                green_tiles.append(Coords(ti.x, ri))
+            top, bottom = min(ti.y, top), max(ti.y, bottom)
+        else:
+            vertical.append((Coords(last.x, top), Coords(last.x, bottom)))
+            top, bottom = ti.y, ti.y
         last = ti
+    vertical.append((Coords(last.x, top), Coords(last.x, bottom)))
 
-    print(*contour, sep="\n")
-    largest_area = find_largest_color(tiles, grid)
+    print("Vertical")
+    print(*vertical, sep="\n")
 
+    largest_area = find_largest_color(red_tiles, green_tiles)
+
+    # render contour
+    xs, ys = zip(*green_tiles)
+    plt.scatter(xs, ys, s=0.1, color="green")
+    xs, ys = zip(*red_tiles)
+    plt.scatter(xs, ys, s=1, color="red")
+    plt.gca().set_aspect("equal", "box")
+    plt.gca().invert_yaxis()
+    plt.savefig(f"{filename}.png", dpi=600)
+    plt.close()
+
+    raise RuntimeError("stop")
     return largest_area
 
 
